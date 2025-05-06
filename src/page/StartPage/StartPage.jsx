f143f2e3ccd55c1a1ef9b3fa135bd8d1f1faae46
씨양 <ssiyang@ssiyang-ui-MacBookPro.local>
2025-05-07 04:33:25 +0900

    Desing

--- src/page/StartPage/StartPage.jsx
+++ src/page/StartPage/StartPage.jsx
@@ -15,7 +15,7 @@ const StartPage = () => {
         <div className="start-stars"></div> {/* ⭐ 추가! */}
         <div className="start-image-wrapper">
             <img src={pxArt} alt="달과 구름" className="start-image" />
-            <h1 className="start-overlay-text">나의 마음은 어떤 영화와 어울릴까?</h1>
+            <h1 className="start-overlay-text">당신의 오늘, 어떤 영화와 닮아있을까요? </h1>
         </div>
       <button onClick={handleStart} className="start-button">
         테스트 시작하기
