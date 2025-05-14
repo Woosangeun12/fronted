d000d4a16ae9bb3a47d99fe72899dbc8b0a9561a
씨양 <ssiyang@ssiyang-ui-MacBookPro.local>
2025-05-14 11:33:06 +0900

    rese

--- src/page/ReviewWritePage/ReviewWritePage.jsx
+++ src/page/ReviewWritePage/ReviewWritePage.jsx
@@ -78,12 +78,8 @@ export default function ReviewWritePage() {
         </label>
 
         <div className="button-group">
-          <button type="submit" className="submit-btn">
-            <span className="btn-text">리뷰 제출</span>
-          </button>
-          <button type="button" className="skip-btn" onClick={handleSkip}>
-            <span className="btn-text">작성 건너뛰기</span>
-          </button>
+          <button type="submit" className="submit-btn">리뷰 제출</button>
+          <button type="button" className="skip-btn" onClick={handleSkip}>작성 건너뛰기</button>
         </div>
       </form>
     </div>
