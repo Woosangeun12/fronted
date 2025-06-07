4bdca76f31fa45bad3b07c6b506b40d24eb6ea1a
씨양 <ssiyang@ssiyang-ui-MacBookPro.local>
2025-06-07 17:24:44 +0900

    dash

--- src/page/AdminReviewPage/AdminReviewPage.jsx
+++ src/page/AdminReviewPage/AdminReviewPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../utils/api';
-import './AdminReviewDashboard.css';
+import './AdminReviewPage.css';
 
 const AdminReviewPage = () => {
   const [reviews, setReviews] = useState([]);
