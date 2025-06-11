003e5c7f1f25dc8bfddd3c38ea8b77a2c680d794
씨양 <ssiyang@ssiyang-ui-MacBookPro.local>
2025-06-11 18:20:16 +0900

    rerere

--- src/page/QuotePage/QuotePage.jsx
+++ src/page/QuotePage/QuotePage.jsx
@@ -117,7 +117,7 @@ export default function QuotePage() {
       }
   
       .emotion-label {
-        font-size: 1.85rem;
+        font-size: 1.8rem;
       }
   
       .question {
@@ -275,9 +275,12 @@ export default function QuotePage() {
         </button>
       </div>
       <div>
-      <button className="lastpage-button" onClick={handleKakaoShare}>
-        내 마음 카카오톡으로 공유하기
-      </button>
+      <div className="button-group">
+        <button className="lastpage-button" onClick={handleKakaoShare}>
+          <div className="button-text-main">내 마음 카카오톡으로 공유하기</div>
+          <div className="button-text-sub">내가 고른 영화와 마음처방전, 친구에게도 전해보세요.</div>
+        </button>
+      </div>
       </div>
     </div>
   );
